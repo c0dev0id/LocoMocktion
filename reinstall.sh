1b62fd2dd2ed0ac808b1ed1780a220b98da15f5c
c0dev0id <sh+git@codevoid.de>
2026-02-17 13:59:25 +0100

    Install Script

---
+++ reinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+rm -f app-debug.apk
+gh run download $(gh run list --workflow Build --status success -L1 --json databaseId --jq '.[].databaseId') --name app-debug
+
+adb uninstall com.locomocktion
+adb install app-debug.apk
+
+# Permissions
+adb shell pm grant com.locomocktion android.permission.POST_NOTIFICATIONS \
+    android.permission.FOREGROUND_SERVICE_LOCATION \
+    android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS \
+    android.permission.ACCESS_COARSE_LOCATION \
+    android.permission.ACCESS_FINE_LOCATION
+
+adb shell appops set com.locomocktion android:mock_location allow
+
+# adb shell dumpsys package com.locomocktion | grep permission
